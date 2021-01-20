4649c80ce30d5d5804c77a3cae54b28f91eb021f
Carlos Torrealba <calrrox@gmail.com>
2021-01-20 11:16:23 -0400

    All done

--- lib/ruby_db/person.rb
+++ lib/ruby_db/person.rb
@@ -3,9 +3,28 @@ require_relative '../../db/connection'
 class Person
   attr_reader :name, :age
   @@connection = Connection.new
+  COLUMNS = [:name, :age]
 
   def self.all
-    @@connection.execute
+    group = []
+    people = []
+    @@connection.db.execute "select * from person" do |row|
+      group << row
+    end
+
+    group.each do |row|
+      attributes = {}
+
+      row.each_with_index do |value, index|
+        attributes[COLUMNS[index]] = value
+      end
+
+      people << Person.new(attributes)
+    end
+    people
+  end
+
+  def self.find(id)
     []
   end
 
@@ -28,4 +47,10 @@ class Person
   end
 end
 
-p Person.all
+people = Person.all
+
+people.first.update(name: 'carlos')
+
+people.first.show
+
+people.first.destroy
